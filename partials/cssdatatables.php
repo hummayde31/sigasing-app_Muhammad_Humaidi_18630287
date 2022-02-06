0ff5ad07e1fac3c779e7dbd3c86eff87124fc36a
hummayde31 <62832964+hummayde31@users.noreply.github.com>
2022-02-07 03:16:02 +0800

    Koneksi database dan datatables

---
+++ partials/cssdatatables.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="plugin/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="plugin/datatables-responsive/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" href="plugin/datatables-button/css/button.bootstrap4.min.css">
