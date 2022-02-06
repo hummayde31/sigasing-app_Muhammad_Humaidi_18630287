db16a1f6bedf75628fe21d675cd187ff2413da91
hummayde31 <62832964+hummayde31@users.noreply.github.com>
2022-02-07 03:50:55 +0800

    Menampilkan halaman read lokasi

--- routes.php
+++ routes.php
@@ -3,6 +3,10 @@ if (isset($_GET['page'])) {
     $page = $_GET['page'];
     switch ($page) {
         case '':
+        case 'lokasiread':
+        file_exists('pages/admin/lokasiread.php') ? include
+        'pages/admin/lokasiread.php' : include "pages/404.php";
+            break;
         case 'home':
             file_exists('pages/home.php') ? include 'pages/home.php': include "pages/404";
             break;
