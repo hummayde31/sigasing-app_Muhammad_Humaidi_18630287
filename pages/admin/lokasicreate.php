053bdfdd2975eac7abfc920a3bc28d5f37d5cd1d
hummayde31 <62832964+hummayde31@users.noreply.github.com>
2022-02-12 23:26:35 +0800

    Membuat Perintah Create Data

---
+++ pages/admin/lokasicreate.php
@@ -0,0 +1,69 @@
+<?php include_once "partials/scripts.php" ?>
+<?php
+if (isset($_POST['button_create'])) {
+
+    $database = new Database();
+    $db = $database->getConnection();
+    $validateSql = "SELECT * FROM lokasi WHERE nama_lokasi = ?";
+    $stmt = $db->prepare($validateSql);
+    $stmt->bindParam(1, $_POST['nama_lokasi']);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+?>
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
+            <h5><i class="icon fas fa-ban"></i></h5>
+            Nama Lokasi Sudah Ada
+        </div>
+<?php
+    } else {
+        $insertSql = "INSERT INTO lokasi SET nama_lokasi = ?";
+        $stmt = $db->prepare($insertSql);
+        $stmt->bindParam(1, $_POST['nama_lokasi']);
+        if ($stmt->execute()) {
+            $_SESSION['hasil'] = true;
+            $_SESSION['pesan'] = "Berhasil simpan data";
+        } else {
+            $_SESSION['hasil'] = false;
+            $_SESSION['pesan'] = "Gagal simpan data";
+        }
+        echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'>";
+    }
+}
+?>
+
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb2">
+            <div class="col-sm-6">
+                <h1>Tambah Data Lokasi</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrump-item"><a href="?page=home">Home </a> /</li>
+                    <li class="breadcrump-item"><a href="?page=lokasiread">Lokasi </a> /</li>
+                    <li class="breadcrump-item">Tambah Data</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="content">
+    <div class="card-header">
+        <h3 class="card-title">Tambah Lokasi</h3>
+    </div>
+    <div class="card-body">
+        <form method="POST">
+            <div class="form-group">
+                <label for="nama_lokasi">Nama Lokasi</label>
+                <input type="text" class="form-control" name="nama_lokasi">
+            </div>
+            <a href="?page=lokasiread" class="btn btn-danger btn-sm float-right">
+                <i class="fa fa-times"></i> Batal
+            </a>
+            <button type="submit" name="button_create" class="btn btn-success btn-sm float-right">
+                <i class="fa fa-save"></i>Simpan
+            </button>
+        </form>
+    </div>
+</section>
